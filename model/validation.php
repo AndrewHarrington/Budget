17046e9fe6a2741015f4f6d9e7ed5889eab1dfa0
Sang Le <lesan6021@gmail.com>
2019-05-17 14:03:56 -0400

    Add validation functions

---
+++ model/validation.php
@@ -0,0 +1,10 @@
+<?php
+// Name validation check for string
+function validName($name) {
+    return isset($name) && $name != "" && ctype_alpha($name);
+}
+
+// Email validation filter valid email
+function validEmail($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
