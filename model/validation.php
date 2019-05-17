fb9bae69012170e1ce805b0d1a521667cf5633e4
Sang Le <lesan6021@gmail.com>
2019-05-17 14:52:32 -0400

    Add password confirmation

--- model/validation.php
+++ model/validation.php
@@ -8,3 +8,27 @@ function validName($name) {
 function validEmail($email) {
     return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
+
+// Password validation
+function validPassword($password, $confirmation) {
+    if(!empty($password) && ($password == $confirmation)) {
+        if(strlen($password) <= 8) {
+            return "Your password must contain at least 8 characters!";
+        }
+        else if(!preg_match("#[0-9]+#",$password)) {
+            return "You password must contain at least 1 number!";
+        }
+        else if(!preg_match("#[A-Z]+#",$password)) {
+            return "Your password must contain at least 1 capital letter!";
+        }
+        else if(!preg_match("#[a-z]+#",$password)) {
+            return "Your password must contain at least 1 lowercase letter!";
+        }
+    }
+    else if(!empty($password)) {
+        return "Please check your confirmation";
+    }
+    else {
+        return "Please enter your password";
+    }
+}
