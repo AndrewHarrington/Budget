8589210942bcfccac8b6a87d3c8630d15ce21187
Andrew Harrington <aharrington4@mail.greenriver.edu>
2019-05-31 22:28:46 -0400

    Added route structure and validation on the pay route

--- index.php
+++ index.php
@@ -35,12 +35,81 @@ $f3->route('GET|POST /registration', function() {
     echo $view->render('views/register.html');
 });
 
-$f3->route('GET|POST /pay', function (){
-    //validation
+$f3->route('GET|POST /pay', function ($f3){
 
+    //TODO: Add inline errors
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //validation
+        $valid = true;
+        $payOBJ = null;
+
+        //determine the type of pay that we received
+        switch ($_POST['type']){
+            //if hourly
+            case 'hor' :
+                //remove undesired characters
+                $wage = preg_replace("/[^0-9.]/", "", $_POST['wage']);
+                $hours = preg_replace("/[^0-9.]/", "", $_POST['hours']);
+                $tax = preg_replace("/[^0-9.]/", "", $_POST['tax']);
+
+                //check validity
+                if(!validNum($wage) || !validNum($hours) || !validNum($tax)){
+                    $valid = false;
+                }
+                else{
+                    //create the object
+                    $payOBJ = new Hourly($hours, $wage, $tax);
+                }
+                break;
+            //if monthly
+            case 'mon':
+                $pay = preg_replace("/[^0-9.]/", "", $_POST['pay']);
+                $tax = preg_replace("/[^0-9.]/", "", $_POST['tax']);
+
+                if(!validNum($pay) || !validNum($tax)){
+                    $valid = false;
+                }
+                else{
+                    $payOBJ = new Salary($pay, $tax);
+                }
+                break;
+            //if manual
+            case 'man':
+                $pay = preg_replace("/[^0-9.]/", "", $_POST['pay']);
+                if(!validNum($pay)){
+                    $valid = false;
+                }
+                else{
+                    $payOBJ = new Manual($pay);
+                }
+                break;
+            default:
+                //bad data
+                $valid = false;
+                //redirect
+                $f3->reroute('/pay');
+        }
+
+        //storage
+        $f3->set('pay', $payOBJ);
+
+        if($valid){
+            //reroute
+            $f3->reroute('/expenses');
+        }
+    }
     // Display a view
     $view = new Template();
     echo $view->render('views/PayTemplate.html');
+});
+
+$f3->route('GET|POST /expenses', function(){
+
+});
+
+$f3->route('GET|POST /results', function(){
+
 });
 
 $f3->run();
