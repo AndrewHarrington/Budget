bfac69effb2d71e590a5d636f508d038c98526ea
Sang Le <lesan6021@gmail.com>
2019-06-01 07:03:45 -0400

    Add registration validation

--- model/validation.php
+++ model/validation.php
@@ -1,4 +1,26 @@
 <?php
+function validRegistration() {
+    global $f3;
+    $valid = true;
+
+    if(!validName($f3->get('fname'))) {
+        $f3->set("errors['fname']", "Please enter a valid first name");
+        $valid = false;
+    }
+
+    if(!validName($f3->get('lname'))) {
+        $f3->set("errors['fname']", "Please enter a valid last name");
+        $valid = false;
+    }
+
+    if(!validEmail($f3->get('email'))) {
+        $f3->set("errors['email']", "Please enter a valid email");
+        $valid = false;
+    }
+
+    return $valid;
+}
+
 // Name validation check for string
 function validName($name) {
     return isset($name) && $name != "" && ctype_alpha($name);
