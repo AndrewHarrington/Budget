3050b42cb4a551516c579b5aa58bf1db4cf81f4b
Andrew Harrington <aharrington4@mail.greenriver.edu>
2019-05-31 22:53:53 -0400

    Expenses list now updates whenever a new expense is added

--- model/javascript/expenses.js
+++ model/javascript/expenses.js
@@ -1,1 +1,5 @@
 $("#expense-list").load("model/ajax-expenses.php", {uuid: $("#uuid").val()});
+//reload the list when a new expense is added
+$("#add").on('click', function(){
+    $("#expense-list").load("model/ajax-expenses.php", {uuid: $("#uuid").val()});
+});
